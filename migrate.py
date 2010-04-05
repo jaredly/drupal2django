dd409b0428f23a9f85decfcf1061518d8afc3217
Jared Forsyth <jabapyth@gmail.com>
2010-04-05 14:34:49 -0600

    refactoring, added user import

--- migrate.py
+++ migrate.py
@@ -14,6 +14,7 @@ from html2rst.parse import convert_html
 cleanup = []
 
 def setup(filename):
+    clear_django()
     folder = os.path.dirname(filename)
     name = os.path.basename(filename).split('.')[0]
     fp, filen, desc = imp.find_module(name, [folder])
@@ -23,84 +24,108 @@ def setup(filename):
         if fp:
             fp.close()
     setup_environ(mod)
+    from django.conf import settings
+    settings._setup()
 
-def setup_hack(dct):
-    fname = rfilename()
-    open(fname+'.py','w').write('''
-DATABASE_ENGINE = '%s'
-DATABASE_NAME = '%s'
-DATABASE_USER = '%s'
-DATABASE_PASSWORD = '%s'
-''' % (dct['engine'], dct.get('name',''), dct.get('user',''), dct.get('pass','')))
-    settings = __import__(fname)
+def clear_db():
+    from basic.blog.models import Post
+    from django.contrib.redirects.models import Redirect
+    Post.objects.all().delete()
+    Redirect.objects.all().delete()
 
-    setup_environ(settings)
-    os.remove(fname+'.py')
-    cleanup.append(fname+'.pyc')
+def drupal_node_to_django(node, revision, slug, user):
+    from basic.blog.models import Post
+    dct = {'slug':slug,'title':node.title}
+    text,skipped = convert_html(revision.body)
+    stext, sk2 = convert_html(revision.teaser)
+    if skipped or sk2:
+        print 'Body not totally parsed', node.title
+        text = revision.body
+        stext = revision.teaser
+        dct['format'] = 'html'
+    else:
+        dct['format'] = 'rest'
+    dct['body'] = text
+    dct['tease'] = stext
+    dct['status'] = node.status + 1
+    dct['created'] = makedtime(node.created)
+    dct['publish'] = makedtime(node.changed)
+    dct['modified'] = makedtime(node.changed)
+    dct['allow_comments'] = node.comment == 2
+    dct['author'] = user
+    # no tags, categories, objects, or author yet.
+    post = Post(**dct)
+    return post
 
 def migrate(drupals, djangos):
     setup(drupals)
 
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-    from drupal2django.models import Node, NodeRevisions, UrlAlias
+    from drupal2django.models import Users, Node, NodeRevisions, UrlAlias
     sys.path.pop(0)
+
+    nodes = get_nodes(Node, NodeRevisions)
+    users = Users.objects.all()
+
+    urls = list(UrlAlias.objects.all())
+
+    setup(djangos)
+    from django.conf import settings
+    clear_db()
+
+    django_save_users(users)
+    django_save_nodes(nodes, users, urls)
+
+def django_save_users(users):
+    from django.contrib.auth.models import User
+    current = set(u.username for u in User.objects.all())
+    for user in users:
+        if not user.name:continue
+        if user.name in current:
+            # print 'Skipping import or %s: name conflict' % user.name
+            continue
+        dct = {'username':user.name, 'email':user.mail, 'password':user.pass_field,
+                'last_login':makedtime(user.login),
+                'date_joined':makedtime(user.created)}
+        print 'saving',user.name
+        User(**dct).save()
+
+def django_save_nodes(nodes, users, urls):
+    from django.contrib.auth.models import User
+    url_dict = dict((url.src,url) for url in urls)
+    user_dict = dict((user.uid,user.name) for user in users)
+    for node, revision in nodes:
+        path = url_dict.get('node/%d' % node.nid, None)
+        if not path:
+            slug = node.title.replace(' ','-').replace('/','-')
+        else:
+            slug = path.dst.split('/')[-1]
+        
+        user = User.objects.get(username=user_dict[node.uid])
+
+        post = drupal_node_to_django(node, revision, slug, user)
+        post.save()
+        drupal_redirect_django(path, post)
+    
+
+def get_nodes(Node, NodeRevisions):
     nodes = []
     for node in Node.objects.all().filter(type="blog"):
         recent = NodeRevisions.objects.all().filter(nid=node.nid).order_by('-timestamp')[0]
         nodes.append([node,recent])
-    urls = list(UrlAlias.objects.all())
-    url_dict = dict((url.src,url) for url in urls)
+    return nodes
 
-    clear_django()
-
-    setup(djangos)
-    from basic.blog.models import Post
+def drupal_redirect_django(path, post):
     from django.contrib.redirects.models import Redirect
-    from django.conf import settings
     from django.contrib.sites.models import Site
-    Post.objects.all().delete()
-    Redirect.objects.all().delete()
-    posts = list(Post.objects.all())
-
-    for node, revision in nodes:
-        path = url_dict.get('node/%d' % node.nid, None)
-        if not path:
-            print 'no path',node.nid, node.title
-            slug = node.title.replace(' ','-').replace('/','-')
-        else:
-            slug = path.dst.split('/')[-1]
-        dct = {'slug':slug,'title':node.title}
-        # TODO add user conversion
-        text,skipped = convert_html(revision.body)
-        stext, sk2 = convert_html(revision.teaser)
-        if skipped or sk2:
-            print 'Body not totally parsed', node.title
-            text = revision.body
-            stext = revision.teaser
-            dct['format'] = 'html'
-        else:
-            dct['format'] = 'rest'
-        dct['body'] = text
-        dct['tease'] = stext
-        dct['status'] = node.status + 1
-        dct['created'] = makedtime(node.created)
-        dct['publish'] = makedtime(node.changed)
-        dct['modified'] = makedtime(node.changed)
-        dct['allow_comments'] = node.comment == 2
-        # no tags, categories, objects, or author yet.
-        post = Post(**dct)
-        post.save()
-        post.created = makedtime(node.created)
-        post.publish = makedtime(node.changed)
-        post.modified = makedtime(node.changed)
-        current = Site.objects.get_current()
-        if path:
-            dst = '/' + path.dst.strip('/') + '/'
-            r = Redirect(site=current, old_path=dst, new_path='/example'+post.get_absolute_url())
-            r.save()
-            src = '/' + path.src.strip('/') + '/'
-            r = Redirect(site=current, old_path=src, new_path='/example'+post.get_absolute_url())
-            r.save()
+    current = Site.objects.get_current()
+    if path:
+        dst = '/' + path.dst.strip('/') + '/'
+        r = Redirect(site=current, old_path=dst, new_path='/example'+post.get_absolute_url())
+        r.save()
+        src = '/' + path.src.strip('/') + '/'
+        r = Redirect(site=current, old_path=src, new_path='/example'+post.get_absolute_url())
+        r.save()
 
 def makedtime(secs):
     return datetime.datetime(*time.localtime(secs)[:-3])
--- models.py
+++ models.py
@@ -56,7 +56,29 @@ class TermNode(models.Model):
     tid = models.IntegerField(primary_key=True)
     class Meta:
         db_table = u'term_node'
-'''
+
+class Users(models.Model):
+    uid = models.IntegerField(primary_key=True)
+    name = models.CharField(unique=True, max_length=180)
+    pass_field = models.CharField(max_length=96, db_column='pass') # Field renamed because it was a Python reserved word. Field name made lowercase.
+    mail = models.CharField(max_length=192, blank=True)
+    mode = models.IntegerField()
+    sort = models.IntegerField(null=True, blank=True)
+    threshold = models.IntegerField(null=True, blank=True)
+    theme = models.CharField(max_length=765)
+    signature = models.CharField(max_length=765)
+    created = models.IntegerField()
+    access = models.IntegerField()
+    login = models.IntegerField()
+    status = models.IntegerField()
+    timezone = models.CharField(max_length=24, blank=True)
+    language = models.CharField(max_length=36)
+    picture = models.CharField(max_length=765)
+    init = models.CharField(max_length=192, blank=True)
+    data = models.TextField(blank=True)
+    class Meta:
+        db_table = u'users'
+
 class Files(models.Model):
     fid = models.IntegerField(primary_key=True)
     uid = models.IntegerField()
@@ -68,6 +90,5 @@ class Files(models.Model):
     timestamp = models.IntegerField()
     class Meta:
         db_table = u'files'
-'''
 
 # vim: et sw=4 sts=4
